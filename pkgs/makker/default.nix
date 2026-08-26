157338f50a23152fc63149f2949503638d66d058
Pivok <pivoc@protonmail.com>
2026-08-26 23:21:04 +0200

    Update makker to 2.0

--- pkgs/makker/default.nix
+++ pkgs/makker/default.nix
@@ -1,28 +1,40 @@
 {
+  lib,
   stdenv,
   fetchFromGitHub,
-  lib,
-  zig
+  makeWrapper,
+  nushell,
 }:
 
 stdenv.mkDerivation rec {
   pname = "makker";
-  version = "1.2.1";
+  version = "2.0.1";
 
   src = fetchFromGitHub {
     owner = "Pivok7";
     repo = "makker";
     tag = "v${version}";
-    sha256 = "sha256-4ZgkTStfVCnFIGoFjTDIE/m5ida/Hu1yAHQlttq7vjs=";
+    sha256 = "sha256-dFNijxfoZ6U5y9LMYAZi4z9Ng5Fep0MksOktGc4ypeM=";
   };
 
   nativeBuildInputs = [
-    zig.hook
+    makeWrapper
   ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${src}/makker $out/bin
+    cp -r ${src}/templates $out/bin
+    wrapProgram $out/bin/$pname \
+      --prefix PATH : ${
+        lib.makeBinPath [
+          nushell
+        ]
+      }
+  '';
 
   meta = with lib; {
     description = "Project template manager";
-    longDescription = "Easily create templates for c, c++ and zig";
     homepage = "https://github.com/Pivok7/makker";
     license = licenses.mit;
     platforms = platforms.all;
