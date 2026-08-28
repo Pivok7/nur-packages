abefa08e119324ebc48bfc29f977e51fb55a0c99
Pivok <pivoc@protonmail.com>
2026-08-28 23:22:44 +0200

    Update uncom

--- pkgs/uncom/default.nix
+++ pkgs/uncom/default.nix
@@ -1,43 +1,49 @@
 {
-  stdenv,
-  fetchFromGitHub,
-  makeWrapper,
   lib,
-  zig,
+  stdenv,
+  fetchFromRadicle,
+  makeWrapper,
+  nushell,
+  gnutar,
   unzip,
   xz,
   bzip2,
+  gzip,
   p7zip,
+  zstd,
 }:
 
 stdenv.mkDerivation rec {
   pname = "uncom";
-  version = "1.1.0";
+  version = "2.0.0";
 
-  src = fetchFromGitHub {
-    owner = "Pivok7";
-    repo = "uncom";
+  src = fetchFromRadicle {
+    seed = "pivok.radicle.garden";
+    repo = "z48Wnoe5hoPP4MfnUBUmNYaNmuWWF";
     tag = "v${version}";
-    sha256 = "sha256-udG2AFcTQpiApEkUuZDDRM1AQCD16wiQav/S3oY4SIk=";
+    sha256 = "sha256-b7lkCEiyMMxG2rltlvPiUNwrG4enmo6IbXpCqJpNEoM=";
   };
 
   nativeBuildInputs = [
     makeWrapper
-    zig.hook
   ];
 
   dontUseZigInstall = true;
 
   installPhase = ''
     mkdir -p $out/bin
-    mv zig-out/bin/uncom $out/bin/
+    cp ${src}/uncom $out/bin
     wrapProgram $out/bin/$pname \
       --prefix PATH : ${
         lib.makeBinPath [
+          nushell
+          gnutar
           unzip
           xz
           bzip2
+          gzip
           p7zip
+          zstd
         ]
       }
   '';
